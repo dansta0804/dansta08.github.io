79b687d5a8aa63adee9912b15ca214251535844c
Danielė <97364205+dansta0804@users.noreply.github.com>
2025-10-19 19:03:37 +0300

    Pridėta prisijungimo lango logika.

--- Application/Scripts/App/server.R
+++ Application/Scripts/App/server.R
@@ -25,9 +25,9 @@ VALIDATIONS       <- paste0(SCRIPTS, "validations.R")
 INPUT             <- paste0(PROJECT, "Input/")
 DATABASES         <- paste0(PROJECT, "Databases/")
 MODULES           <- paste0(APP, "Modules/")
-RESEARCHER_MODULE <- paste0(MODULES, "Researcher/")
 PATIENT_MODULE    <- paste0(MODULES, "Patient/")
 DOCTOR_MODULE     <- paste0(MODULES, "Doctor/")
+RESEARCHER_MODULE <- paste0(MODULES, "Researcher/")
 
 print(RESEARCHER_MODULE)
 
@@ -56,8 +56,8 @@ server <- function(input, output, session) {
             textInput("name", "Vardas:"),
             textInput("surname", "Pavardė:"),
             textInput("personal_code", "Asmens kodas:"),
-            textInput("email", "El. pašto adresas:"),
-            textInput("phone", "Tel. numeris:"),
+            textInput("signup_email", "El. pašto adresas:"),
+            textInput("signup_phone", "Tel. numeris:"),
             textInput("address", "Adresas:"),
             selectInput("gender", "Lytis:",
                         list("Moteris" = "Moteris", "Vyras" = "Vyras",
@@ -78,12 +78,31 @@ server <- function(input, output, session) {
           )
         )
       )
+    ),
+    tabPanel(
+      "Prisijungimas",
+      useShinyFeedback(),
+      fluidRow(
+        column(4, offset = 4,
+          wellPanel(
+            p(class = "titles", "Prisijungimas prie sistemos"),
+            textInput("login_email", "El. pašto adresas:"),
+            textInput("login_phone", "Tel. numeris:"),
+            passwordInput("password", "Slaptažodis:"),
+            actionButton("login_btn", "Prisijungti",
+                        class = "btn-primary btn-block"),
+            br(), br(),
+            textOutput("login_message")
+          )
+        )
+      )
     )
   )})
 
-  ############### REGISTRACIJOS LANGO LOGIKA ###############
+  ######################### REGISTRACIJOS LANGO LOGIKA #########################
   observeEvent(input$signup_btn, {
-    con <- dbConnect(RMySQL::MySQL(), dbname = "GeneticDataExchangeSystem",
+    con <-
+      dbConnect(RMySQL::MySQL(), dbname = "GeneticDataExchangeSystem",
                 host = "127.0.0.1", user = "root",
                 password = "daniele_Vostro000804",
                 client.flag = CLIENT_MULTI_STATEMENTS + CLIENT_LOCAL_FILES)
@@ -91,8 +110,8 @@ server <- function(input, output, session) {
     ERRORS <- c(ERRORS, validate_name(input$name))
     ERRORS <- c(ERRORS, validate_surname(input$surname))
     ERRORS <- c(ERRORS, validate_personal_code(input$personal_code))
-    ERRORS <- c(ERRORS, validate_email(input$email))
-    ERRORS <- c(ERRORS, validate_phone(input$phone))
+    ERRORS <- c(ERRORS, validate_email("signup_email", input$signup_email))
+    ERRORS <- c(ERRORS, validate_phone("signup_phone", input$signup_phone))
     ERRORS <- c(ERRORS, validate_address(input$address))
     ERRORS <- c(ERRORS, validate_gender(input$gender))
     ERRORS <- c(ERRORS, validate_role(input$role))
@@ -103,7 +122,8 @@ server <- function(input, output, session) {
         c(ERRORS, validate_text("institution_doc", input$institution_doc))
       ERRORS <-
         c(ERRORS, validate_text("specialization", input$specialization))
-    } else if (input$role == "Tyrėjas") {
+    }
+    else if (input$role == "Tyrėjas") {
       ERRORS <-
         c(ERRORS, validate_text("institution_res", input$institution_res))
     }
@@ -113,8 +133,8 @@ server <- function(input, output, session) {
         Vardas = input$name,
         Pavarde = input$surname,
         Asmens_kodas = input$personal_code,
-        El_pastas = input$email,
-        Tel_numeris = input$phone,
+        El_pastas = input$signup_email,
+        Tel_numeris = gsub(" ", "", input$signup_phone),
         Adresas = input$address,
         Lytis = input$gender,
         Kategorija = input$role,
@@ -127,10 +147,16 @@ server <- function(input, output, session) {
         showNotification("Naudotojas sėkmingai sukurtas!", type = "message",
                          closeButton = TRUE, duration = 2)
 
-        if (input$role == "Tyrėjas") {
-          source(paste0(RESEARCHER_MODULE, "researcher_ui.R"), local = TRUE)
+        if (input$role == "Gydytojas") {
+          source(paste0(DOCTOR_MODULE, "ui.R"), local = TRUE)
+          output$main_ui <- renderUI({doctor_ui("doctor")})
+          source(paste0(DOCTOR_MODULE, "server.R"), local = TRUE)
+          callModule(doctor_server, "doctor")
+        }
+        else if (input$role == "Tyrėjas") {
+          source(paste0(RESEARCHER_MODULE, "ui.R"), local = TRUE)
           output$main_ui <- renderUI({researcher_ui("researcher")})
-          source(paste0(RESEARCHER_MODULE, "researcher_server.R"), local = TRUE)
+          source(paste0(RESEARCHER_MODULE, "server.R"), local = TRUE)
           callModule(researcher_server, "researcher")
         }
       }, error = function(e) {
@@ -142,6 +168,53 @@ server <- function(input, output, session) {
       showNotification("Klaida registruojant naudotoją!", type = "error",
                         closeButton = TRUE, duration = 2)
     }
+  })
+
+  ######################### PRISIJUNGIMO LANGO LOGIKA ##########################
+  observeEvent(input$login_btn, {
+    con <-
+      dbConnect(RMySQL::MySQL(), dbname = "GeneticDataExchangeSystem",
+                host = "127.0.0.1", user = "root",
+                password = "daniele_Vostro000804",
+                client.flag = CLIENT_MULTI_STATEMENTS + CLIENT_LOCAL_FILES)
+
+    if (input$login_phone == "" && input$login_email != "") {
+        ERRORS <- c(ERRORS, validate_email("login_email", input$login_email))
+      }
+      if (input$login_phone != "" && input$login_email == "") {
+        ERRORS <- c(ERRORS, validate_phone("login_phone", input$login_phone))
+      }
+      if (input$login_phone == "" && input$login_email == "") {
+        ERRORS <- c(ERRORS, phone_email_exists("login_phone", input$login_phone))
+        ERRORS <- c(ERRORS, phone_email_exists("login_email", input$login_email))
+      }
+
+      else {
+        ERRORS <- c(ERRORS, validate_email("login_email", input$login_email))
+        ERRORS <- c(ERRORS, validate_phone("login_phone", input$login_phone))
+      }
+    
+    
+    if (length(ERRORS) == 0) {
+      tryCatch({
+        user <-
+          dbGetQuery(con,
+                     paste0("SELECT Vardas FROM ASMUO WHERE Tel_numeris = '",
+                     input$login_phone, "';")
+          )
+      }, error = function(e) {
+        print(e)
+        
+      }, finally = {
+        dbDisconnect(con)
+      })
+    } else {
+      showNotification("Klaida bandant prisijungti!", type = "error",
+                        closeButton = TRUE, duration = 2)
+    }
+    
+    
+    
   })
 
   output$extraFields <- renderUI({
